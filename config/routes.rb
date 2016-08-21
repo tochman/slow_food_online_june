ba45df6f682fe55f21649cddf2ad8c7b8603a5c5
lollypop27 <lara@adfactory.co.za>
2016-08-21 10:16:46 +0200

    updated path to be shorter

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'restaurant_create/restaurant_create', to: 'restaurant_create#restaurant_create', as: 'restaurant_create'
+
   get 'landing_page/index'
 
   root 'landing_page#index'
