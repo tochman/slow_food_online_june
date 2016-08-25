b7a32e7e0609d00be08a24c4d34333df872f0afb
Raoul DIFFOUO <diraulo@gmail.com>
2016-08-25 14:37:08 +0200

    Fix lara's PR (#22)
    * adding JS fixed footer
    
    * adding JS fixed footer
    
    * recreated db with updated fields
    
    took out semicolon in css file
    updated Login In to Login
    added copyright and updated text formatting
    add foundation.css to assets.rb in order to be called
    as main css file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,27 @@
 ///= require_tree .
 
 $(function(){ $(document).foundation(); });
+
+function fixedFooter() {
+    var footer = $(".footer"); //or your footer class
+    height = footer.height();
+    paddingTop = parseInt(footer.css('padding-top'), 10);//zero if is undefined
+    paddingBottom = parseInt(footer.css('padding-bottom'), 0);//zero if is undefined
+    totalHeight = (height + paddingTop + paddingBottom);
+    footerPosition = footer.position();
+    windowHeight = $(window).height();
+    height = (windowHeight - footerPosition.top) - totalHeight;
+    if (height > 0) {
+      footer.css({
+        'margin-top': (height) + 'px'
+      });
+    }
+  }
+
+  $(document).ready(function () {
+    fixedFooter(); //at page load
+
+  });
+  $(window).resize(function () {
+    fixedFooter(); //at page resize
+  });
