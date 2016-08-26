9c035377890b468555f1e198f94734a1f7861f5c
Lucia De Mojà <luciademoja@gmail.com>
2016-08-26 11:19:56 +0200

    adds scenario creates a dish

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -3,21 +3,21 @@ class DishesController < ApplicationController
     @dish = Dish.new
   end
 
-  # def create
-  #   @menu = Dishes.new(dish_params)
-  #   if @dish.save
-  #     redirect_to menus_path
-  #   else
-  #     flash[:error] = 'A name is required'
-  #     redirect_to new_dish_path
-  #   end
-  # end
-  #
-  # private
-  #
-  # def dish_params
-  #   params.require(:dish).permit(:name, :price)
-  # end
+  def create
+    @dish = Dish.new(dish_params)
+    if @dish.save
+      redirect_to root_path
+    else
+      flash[:error] = 'A name is required'
+      redirect_to new_dish_path
+    end
+  end
+
+  private
+
+  def dish_params
+    params.require(:dish).permit(:name, :price, :allergy_info, :ingredients, :calories)
+  end
 
 
 end
--- features/create_dishes.feature
+++ features/create_dishes.feature
@@ -2,12 +2,12 @@ Feature: As a system user
   So that I can fill my menu with dishes
   I want to be able to create dishes
 
-  Scenario: Create a list of dishes offered
+  Scenario: Creates a dish
     Given I am on the "create dish" page
     And I fill "Name" with "Dish Name"
     And I fill "Price" with "3.00"
-    And I fill "Allergy Information" with "Peanuts"
-    And I fill "Incredients" with "Mince"
+    And I fill "Allergy info" with "Peanuts"
+    And I fill "Ingredients" with "Mince"
     And I fill "Calories" with "429"
     And I click "Create"
     Then I should be on the "home" page
